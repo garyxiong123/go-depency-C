b63b34aec36eb22e7cd9172f34e602bd34509fe3
garyxiong123 <502238410@qq.com>
2023-01-25 19:19:45 +0800

    init

--- C/b.go
+++ C/b.go
@@ -2,11 +2,9 @@ package B
 
 import (
 	"github.com/garyxiong123/go-depency-A/A2"
-	"github.com/garyxiong123/go-depency-B/B"
 )
 
 func Go_dependency_C_V1() {
-	B.Go_dependency_B_V1()
 
 	A2.Go_dependency_A_V2()
 	println("go_dependency_C_V1")
--- go.mod
+++ go.mod
@@ -1,4 +1,7 @@
 module github.com/garyxiong123/go-depency-C
 
-go 1.17
+go 1.18
 
+require (
+	github.com/garyxiong123/go-depency-A v1.0.6-no-m
+)
