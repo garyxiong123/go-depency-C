6f5dbf96b0947da2972e5ea9e370ef18a955f1a0
garyxiong123 <502238410@qq.com>
2023-01-25 17:23:30 +0800

    init

--- C/b.go
+++ C/b.go
@@ -1,8 +1,13 @@
 package B
 
-import "github.com/garyxiong123/go-depency-B/B"
+import (
+	A "github.com/garyxiong123/go-depency-A"
+	"github.com/garyxiong123/go-depency-B/B"
+)
 
 func Go_dependency_C_V1() {
 	B.Go_dependency_B_V1()
+
+	A.Go_dependency_A_V2()
 	println("go_dependency_C_V1")
 }
